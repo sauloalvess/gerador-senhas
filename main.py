6f03960c1727a0bfd6e9907f991642a26702335c
Saulo Alves <sauloalves@live.com>
2023-01-11 11:38:55 -0300

    Código finalizado

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import random
-import secrets
+import string
+
+print("Gerador de Senhas")
+
+caracteres = string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation
+
+def gerar_senha():
+    tamanho = int(input("Digite o tamanho da senha: "))
+    embaralha = random.sample(caracteres, tamanho)
+    senha = "".join(embaralha)
+
+    print("Sua nova senha:" + senha)
+    
+print(gerar_senha())
